a735a6f2f6c0e17681756fcab6c54fe29f0a59ab
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2022-06-30 15:00:02 +0000

    Add numerics tests

--- tests/test_numcertain.py
+++ tests/test_numcertain.py
@@ -4,6 +4,26 @@ import sys
 from numcertain import __version__
 
 
-def test_cli_version():
+def test_cli_version_shows_version():
     cmd = [sys.executable, "-m", "numcertain", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__
+    assert __version__ == subprocess.check_output(cmd).decode().strip()
+
+
+def test_cli_help_shows_help():
+    cmd = [sys.executable, "-m", "numcertain", "--help"]
+    assert (
+        subprocess.check_output(cmd)
+        .decode()
+        .strip()
+        .startswith("Usage: python -m numcertain")
+    )
+
+
+def test_cli_noargs_shows_help():
+    cmd = [sys.executable, "-m", "numcertain"]
+    assert (
+        subprocess.check_output(cmd)
+        .decode()
+        .strip()
+        .startswith("Usage: python -m numcertain")
+    )
